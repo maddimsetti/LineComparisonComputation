76027bdbb51b6fffc6c7dd348c142de3ea9bd714
prudhvi <prudhvieethakota123@gmail.com>
2021-04-27 00:44:55 +0530

    Added:Welcome Message to Line Comparison Computation Program

---
+++ LineComparsionProblem.java
@@ -0,0 +1,10 @@
+package comparison;
+
+public class LineComparsionProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
