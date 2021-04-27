ccb2c5c4528e65df03d37f384c19a04165e2c906
prudhvi <prudhvieethakota123@gmail.com>
2021-04-27 19:31:51 +0530

    Added: Equality of two lines based on the end Points Checked and I understand When two lines be equal

--- LineComparsionProblem.java
+++ LineComparsionProblem.java
@@ -8,33 +8,58 @@ public class LineComparsionProblem {
 		// TODO Auto-generated method stub
        System.out.println("Welcome to Line Comparison Computation Program");
        
-       int a1,a2,b1,b2,c1,c2;
-       double x,y;
+       double x1,x2,y1,y2,a1,a2,b1,b2;
+       double midPoint1x,midPoint1y,Length1,Length2;
   
 	@SuppressWarnings("resource")
 	Scanner sc=new Scanner(System.in);
        
-       System.out.println("enter the value of a1,b1,c1 of first-line in form of a1x+b1y+c=0\n");
+       System.out.println("enter the value of x1\n");
+       x1=sc.nextInt();
+       
+       System.out.println("enter the value of x2\n");
+       x2=sc.nextInt();
+       
+       System.out.println("enter the value of y1\n");
+       y1=sc.nextInt();
+       
+       System.out.println("enter the value of y2\n");
+       y2=sc.nextInt();
+       
+       midPoint1x = (x1+x2)/2;
+       midPoint1y = (y1+y2)/2;
+       System.out.printf("X coordinate is %.2f\n",midPoint1x);
+       System.out.printf("Y coordinate is %.2f\n",midPoint1y);
+      
+       Length1 = Math.sqrt(Math.pow((midPoint1x - x1),2) + Math.pow((midPoint1y - y1),2));
+       System.out.println("Length of a Line between coordinate and Mid_Point of "+"("+x1+","+y1+"),"+"("+midPoint1x+","+midPoint1y+") is" +Length1);
+       
+       System.out.println("enter the value of a1\n");
        a1=sc.nextInt();
-       b1=sc.nextInt();
-       c1=sc.nextInt();
        
-       System.out.println("enter the values of a2,b2,c2 of second-line in form of a2x+b2y+c2=0\n");
+       System.out.println("enter the value of a2\n");
        a2=sc.nextInt();
+       
+       System.out.println("enter the value of b1\n");
+       b1=sc.nextInt();
+       
+       System.out.println("enter the value of b2\n");
        b2=sc.nextInt();
-       c2=sc.nextInt();
        
-       x=(((b1*c2)-(b2*c1))/((a1*b2)-(a2*b1)));
-       y=(((a2*c1)-(a1*c2))/((a1*b2)-(a2*b1)));
-       System.out.printf("X coordinate is %,.2f\n:",x);
-       System.out.printf("Y coordinate is %,.2f\n:",y);
+       double midPoint2x,midPoint2y;
        
-	   double firstLine=(a1*x)+(b1*y)+c1;
-	   double secondLine=(a2*x)+(b2*y)+c2;
-	   
-	   String str1 = Double.toString(firstLine);
-	   String str2 = Double.toString(secondLine);
-	   
-	   System.out.println(firstLine + " .equals " + secondLine + ":" + str1.equals(str2));
+       midPoint2x = ((a1+a2)/2);
+       midPoint2y = ((b1+b2)/2);
+       System.out.printf("X coordinate is %.2f\n",midPoint2x);
+       System.out.printf("Y coordinate is %.2f\n",midPoint2y);
+       
+       Length2 = Math.sqrt(Math.pow((midPoint2x - a1),2) + Math.pow((midPoint2y - b1),2));
+       System.out.println("Length of a Line between coordinate and Mid_Point of "+"("+a1+","+b1+"),"+"("+midPoint2x+","+midPoint2y+") is" +Length2);
+
+       String str1 = String.valueOf(Length1);
+       String str2 = String.valueOf(Length2);
+       System.out.println(Length1 + " .equals " + Length2
+               + ": " + str1.equals(str2));
+      
 	}
 }
