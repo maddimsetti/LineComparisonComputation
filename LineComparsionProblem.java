802cf485e43d1fd3b84ea8ca2b66232e5df724c7
prudhvi <prudhvieethakota123@gmail.com>
2021-04-27 11:32:29 +0530

    Added:Check the Equality of Two lines based on the End Points, and find Where the lines Equal

--- LineComparsionProblem.java
+++ LineComparsionProblem.java
@@ -7,27 +7,34 @@ public class LineComparsionProblem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Welcome to Line Comparison Computation Program");
-
-       int x1,x2,y1,y2;
-       double lengthOfALine;
-
+       
+       int a1,a2,b1,b2,c1,c2;
+       double x,y;
+  
 	@SuppressWarnings("resource")
 	Scanner sc=new Scanner(System.in);
-
-       System.out.println("enter x1 point\n");
-       x1=sc.nextInt();
-
-       System.out.println("enter y1 point\n");
-       y1=sc.nextInt();
-
-       System.out.printf("enter x2 point\n");
-       x2=sc.nextInt();
-
-       System.out.println("enter y2 point\n");
-       y2=sc.nextInt();
-
-	lengthOfALine = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
-       System.out.println("Length of a Line between two coordinates of "+"("+x1+","+y1+"),"+"("+x2+","+y2+") is" +lengthOfALine);     
+       
+       System.out.println("enter the value of a1,b1,c1 of first-line in form of a1x+b1y+c=0\n");
+       a1=sc.nextInt();
+       b1=sc.nextInt();
+       c1=sc.nextInt();
+       
+       System.out.println("enter the values of a2,b2,c2 of second-line in form of a2x+b2y+c2=0\n");
+       a2=sc.nextInt();
+       b2=sc.nextInt();
+       c2=sc.nextInt();
+       
+       x=(((b1*c2)-(b2*c1))/((a1*b2)-(a2*b1)));
+       y=(((a2*c1)-(a1*c2))/((a1*b2)-(a2*b1)));
+       System.out.printf("X coordinate is %,.2f\n:",x);
+       System.out.printf("Y coordinate is %,.2f\n:",y);
+       
+	   double firstLine=(a1*x)+(b1*y)+c1;
+	   double secondLine=(a2*x)+(b2*y)+c2;
+	   
+	   String str1 = Double.toString(firstLine);
+	   String str2 = Double.toString(secondLine);
+	   
+	   System.out.println(firstLine + " .equals " + secondLine + ":" + str1.equals(str2));
 	}
-
 }
